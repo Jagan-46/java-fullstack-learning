448ff9514b5d33bd3457efb5409e2b76762b3c73
Jagan-46 <jaganirakam6@gmail.com>
2026-08-22 09:29:22 +0530

    Spring Boot Day 4: Query parameters with search by name and filter by marks range endpoints using @RequestParam

---
+++ spring-boot/Day-4/Students/src/main/java/com/example/Students/StudentController.java
@@ -0,0 +1,61 @@
+package com.example.Students;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+@RestController
+public class StudentController {
+    ArrayList<Student> students = new ArrayList<>();
+
+    @PostMapping("/api/students")
+    public String addStudent(@RequestBody Student student) {
+        students.add(student);
+        return "Added";
+    }
+
+    @GetMapping("/api/students")
+    public ArrayList<Student> getAllStudent() {
+        return students;
+    }
+
+    @PutMapping("/api/students/{id}")
+    public String updateStudents(@PathVariable int id, @RequestBody Student student) {
+        for (Student s : students) {
+            if (s.getId() == id) {
+                s.setName(student.getName());
+                s.setId(student.getId());
+                return "Updated";
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/api/students/{id}")
+    public String deleteStudent(@PathVariable int id) {
+        for (Student s : students) {
+            if (s.getId() == id) {
+                students.remove(s);
+                return "Deleted Successfuly";
+            }
+        }
+        return "null";
+    }
+    @GetMapping("/api/students/search")
+    public ArrayList<Student> searchByName(@RequestParam String name){
+        ArrayList<Student> result = new ArrayList<>();
+        for(Student s : students) {
+            if (s.getName().equals(name)) {
+                result.add(s);
+            }
+        }
+        return result;
+    }
+    @GetMapping("/api/students/filter")
+    public ArrayList<Student> filterStudent(@RequestParam int minMarks,@RequestParam int maxMarks){
+        ArrayList<Student> result = new ArrayList<>();
+        for(Student s : students) {
+            if (s.getMarks() >= minMarks && s.getMarks() <= maxMarks) {
+                result.add(s);
+            }
+        }
+        return result;
+    }
+}
