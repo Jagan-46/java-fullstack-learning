bc7bddf541258c48be23ccae63d993530f5ece11
Jagan-46 <jaganirakam6@gmail.com>
2026-09-06 08:23:32 +0530

    Spring Boot Week 4 Complete: Full-featured Student Management API with MySQL database, JPA relationships, transactions, custom exception handling, global error handler, and service layer validation

---
+++ spring-boot/Week-2/Project/Student/src/main/java/com/example/Student/StudentService.java
@@ -0,0 +1,75 @@
+package com.example.Student;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.slf4j.*;
+import java.util.*;
+
+@Service
+public class StudentService {
+    private static final Logger logger = LoggerFactory.getLogger(StudentService.class);
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    TeacherRepository teacherRepository;
+
+    @Transactional
+    public String transferStudent(int studentId, int teacherId) {
+        Student student = studentRepository.findById(studentId).orElse(null);
+        try {
+            if (student == null) {
+                logger.warn("Student not found");
+                throw new StudentNotFoundException("Student with id "+studentId+" not found");
+            }
+
+            Teacher newTeacher = teacherRepository.findById(teacherId).orElse(null);
+            if (newTeacher == null) {
+                logger.warn("Teacher Not Found");
+                throw new TeacherNotFoundException("Teacher with id "+teacherId+" not found");
+            }
+            logger.info("Transforing Student" + student.getName() + newTeacher.getName());
+            student.setTeacher(newTeacher);
+            studentRepository.save(student);
+            logger.info("Transfer Successfuly");
+            return "Student Tansfered Successfuly";
+        } catch (Exception e) {
+            logger.error("Transfer Failed" + e.getMessage());
+            throw new RuntimeException("Transfer Failed" + e.getMessage());
+        }
+    }
+
+    @Transactional
+    public String addStudentWithTeacher(Student student, int teacherId) {
+
+        Teacher teacher = teacherRepository.findById(teacherId).orElse(null);
+        try {
+            if (teacher == null) {
+               throw new TeacherNotFoundException("Teacher with id "+teacherId+" not found");
+            }
+            student.setTeacher(teacher);
+
+            if (teacher.getStudents() == null) {
+                teacher.setStudents(new ArrayList<>());
+            }
+            teacher.getStudents().add(student);
+            studentRepository.save(student);
+            teacherRepository.save(teacher);
+            logger.info("Student added to Teacher: " + teacher.getName());
+            return "Student Added Successfuly";
+        }catch(Exception e){
+            logger.error("Error: "+e.getMessage());
+            throw new RuntimeException("Error: "+e.getMessage());
+        }
+    }
+    public Student getStudentById(int id){
+        logger.info("Getting student with id: "+id);
+        Student student = studentRepository.findById(id).orElse(null);
+        if(student==null) {
+            logger.info("Student not found: "+ id);
+            throw new StudentNotFoundException("Student with id " + id + " not found");
+        }
+        logger.info("Student found: "+student.getName());
+        return student;
+    }
+}
