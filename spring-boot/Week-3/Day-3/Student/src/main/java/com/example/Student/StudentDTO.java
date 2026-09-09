9ed3883833f921b079fc8f0a4628a9a03dd26197
Jagan-46 <jaganirakam6@gmail.com>
2026-09-09 08:16:11 +0530

    Spring Boot Week 5 Day 3: Sorting - Sort.Direction, sortBy and sortDirection parameters, validation of sortBy fields, combine sorting with pagination

---
+++ spring-boot/Week-3/Day-3/Student/src/main/java/com/example/Student/StudentDTO.java
@@ -0,0 +1,34 @@
+package com.example.Student;
+
+public class StudentDTO {
+    private int id;
+    private String name;
+    private int marks;
+
+    public StudentDTO(){
+
+    }
+    public StudentDTO(int id,String name,int marks){
+        this.id=id;
+        this.name=name;
+        this.marks=marks;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getMarks(){
+        return marks;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setMarks(int marks){
+        this.marks=marks;
+    }
+}
